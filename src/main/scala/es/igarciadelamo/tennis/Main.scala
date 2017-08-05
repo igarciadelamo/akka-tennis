ed1d7f8de5cf8bddd7f823f4848886429dbc2e6c
Isidro Garcia <isidrogarcia@paradigmadigital.com>
2017-08-05 13:21:26 +0200

    Adding ActorLogging to actors implementations. Removing code not necessary like new

--- src/main/scala/es/igarciadelamo/tennis/Main.scala
+++ src/main/scala/es/igarciadelamo/tennis/Main.scala
@@ -3,79 +3,81 @@ package es.igarciadelamo.tennis
 import akka.actor._
 import es.igarciadelamo.tennis.Main.system
 
-case class Ball (target: Int)
-case class StartGame(opposite : ActorRef)
-case class EndGame(winner : ActorRef)
+
+case class Ball(target: Int)
+case class StartGame(opposite: ActorRef)
+case class EndGame(winner: ActorRef)
 case object StartMatch
 
 
-class TennisMatch extends Actor {
+class TennisMatch extends Actor with ActorLogging {
 
   val player1 = system.actorOf(Props(new Player(self)), name = "Agassi")
   val player2 = system.actorOf(Props(new Player(self)), name = "Sampras")
 
   def receive = {
     case StartMatch =>
-      player1 ! new StartGame(player2)
+      player1 ! StartGame(player2)
 
-    case w : EndGame =>
-      println("The winner is " + w.winner.path.name)
+    case w: EndGame =>
+      log.info("The winner is " + w.winner.path.name)
       context.stop(self)
   }
 }
 
 
-class Player(tm : ActorRef) extends Actor {
+class Player(tm: ActorRef) extends Actor with ActorLogging {
 
-  var position = 5
   val LOWER_LIMIT = 0
   val UPPER_LIMIT = 10
   val DEVIATION = 4
   val MOVEMENT = 3
 
+  var position = (UPPER_LIMIT - LOWER_LIMIT) / 2
+
   def name = self.path.name
 
-  def hitTheBall : Int = {
-    val rnd = new scala.util.Random
+  def hitTheBall: Int = {
+    val rnd = scala.util.Random
     val left = rnd.nextBoolean()
-    val factor = if(left) -1 else 1
+    val factor = if (left) -1 else 1
     position + factor * rnd.nextInt(DEVIATION)
   }
 
   def thereIsAWinner(b: Ball): Option[ActorRef] = {
-    if(b.target < LOWER_LIMIT || b.target > UPPER_LIMIT){
+    if (b.target < LOWER_LIMIT || b.target > UPPER_LIMIT) {
       Some(self)
-    }else if(position - MOVEMENT > b.target || position + MOVEMENT < b.target ){
+    } else if (position - MOVEMENT > b.target || position + MOVEMENT < b.target) {
       Some(sender)
-    }else {
+    } else {
       None
     }
   }
 
   def receive = {
 
-    case m : StartGame =>
+    case m: StartGame =>
       val where = hitTheBall
-      println(s"First ball. Ball from $name (from $position) send the ball to $where")
+      log.info(s"First ball. Ball from $name (from $position) send the ball to $where")
       m.opposite ! Ball(where)
 
-    case b : Ball => thereIsAWinner(b) match {
-        case Some(a) => tm ! EndGame(a)
-        case None => {
-          val where = hitTheBall
-          val newPosition = b.target
-          println(s"Ball from $name (move from $position to $newPosition) send the ball to $where")
-          position = newPosition
-          sender ! Ball(where)
-        }
+    case b: Ball => thereIsAWinner(b) match {
+      case Some(a) => tm ! EndGame(a)
+      case None => {
+        val where = hitTheBall
+        val newPosition = b.target
+        log.info(s"Ball from $name (move from $position to $newPosition) send the ball to $where")
+        position = newPosition
+        sender ! Ball(where)
       }
+    }
   }
 }
 
 object Main extends App {
 
   val system = ActorSystem("TennisMatch")
-  val tennisMatch = system.actorOf(Props[TennisMatch])
+  val tennisMatch = system.actorOf(Props[TennisMatch], name = "TennisMatch")
   tennisMatch ! StartMatch
 
 }
